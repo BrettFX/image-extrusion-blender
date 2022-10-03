254f636c4e2626868be244d8ca1dd31e8dbdaa07
BrettFX <brettallen777@gmail.com>
2022-10-02 21:58:09 -0400

    Enhance cli args configuration Can now control sobel algorithm, output path, and debug mode from command line args

--- blend.py
+++ blend.py
@@ -6,8 +6,7 @@ import cv2
 from matplotlib import pyplot as plt
 import argparse
 import logging
-
-DEBUG_MODE = False
+import os
 
 __description__ = """Image extrusion script to combine two images together and elevate/extruded
 edges of the second image, blended with the first image. This process
@@ -17,9 +16,14 @@ sum blending algorithm to achieve the desired result."""
 parser = argparse.ArgumentParser(description=__description__, prog="Python Image Extrusion Blender")
 parser.add_argument('background', help='Input background image.')
 parser.add_argument('foreground', help='Input foreground image.')
+parser.add_argument('--sobel-x', '-x', dest='use_sobel_x', action='store_true', default=True, help='Use derivative x value for Sobel calculation. Default True.')
+parser.add_argument('--sobel-y', '-y', dest='use_sobel_y', action='store_true', default=False, help='Use derivative y value for Sobel calculation. Default False.')
+parser.add_argument('--outpath', '-o', default='blended.png', help='Path write blended file to. Default "blended.png".')
+parser.add_argument('--debug', '-d', dest='debug_mode', action='store_true', default=False, help='Run program in debug mode to show verbose logging and display intermediate steps. Default False.')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s Version 1.0')
 
 args = parser.parse_args()
+DEBUG_MODE = args.debug_mode
 
 logging.basicConfig(
   level=logging.INFO if not DEBUG_MODE else logging.DEBUG,
@@ -63,20 +67,28 @@ def display_all(images: list, column_limit=2, cmap="gray") -> None:
     
   plt.show(block=True)
 
-def save_img(img, fname="image.png", cmap="gray", fmt="png"):
-  plt.imsave(fname=fname, arr=img, cmap=cmap, format=fmt)
+def save_img(img, path="image.png", cmap="gray", fmt="png"):
+  plt.imsave(fname=path, arr=img, cmap=cmap, format=fmt)
 
 def main():
   logger.debug("Debug mode enabled.")
   bg_path = args.background
   fg_path = args.foreground
+  use_sobel_x = args.use_sobel_x
+  use_sobel_y = args.use_sobel_y
+
+  if not os.path.exists(bg_path):
+    raise OSError(f"{bg_path} does not exist. Path to an existing image file required.")
+
+  if not os.path.exists(fg_path):
+    raise OSError(f"{fg_path} does not exist. Path to an existing image file required.")
 
   logger.debug(f"Background image path: {bg_path}")
   logger.debug(f"Foreground image path: {fg_path}")
 
   # NOTE: Read as grayscale with cv2.IMREAD_GRAYSCALE as 2nd arg
-  bg_img = cv2.imread('test-background.png')
-  fg_img = cv2.imread('test-foreground.png') 
+  bg_img = cv2.imread(bg_path)
+  fg_img = cv2.imread(fg_path)
 
   logger.debug(f"Background image shape: {bg_img.shape}")
   logger.debug(f"Foreground image shape: {fg_img.shape}")
@@ -85,16 +97,22 @@ def main():
   gray_fg_img = cv2.cvtColor(fg_img, cv2.COLOR_BGR2GRAY)
 
   # Apply Sorbel filter to foreground image
-  # fg_sobel_x = cv2.Sobel(gray_fg_img,cv2.CV_64F,1,0,ksize=7)
-  # fg_sobel_y = cv2.Sobel(gray_fg_img,cv2.CV_64F,0,1,ksize=7)
+  ddepth = cv2.CV_64F          # Output image depth
+  dx = 1 if use_sobel_x else 0 # Order of derivative x
+  dy = 1 if use_sobel_y else 0 # Order of derivative y
+  ks = 7                       # Kernel size
 
-  # TODO Allow user to add preference for using sobel x or y results for extrusion via args
-  extruded_fg_img = cv2.Sobel(gray_fg_img,cv2.CV_64F,1,0,ksize=7) # Sobel X
+  logger.debug("Sobel configuration:")
+  logger.debug(f"|-- ddepth = {ddepth} (cv2.CV_64F)")
+  logger.debug(f"|-- dx     = {dx}")
+  logger.debug(f"|-- dy     = {dy}")
+  logger.debug(f"|-- ks     = {ks}")
+  extruded_fg_img = cv2.Sobel(gray_fg_img, ddepth, dx, dy, ksize=ks)
 
   # Resize foreground image to match the size of the background image
   original_fg_shape = fg_img.shape
   original_extruded_fg_shape = extruded_fg_img.shape
-  h, w, c = bg_img.shape # Extract height (rows), width (cols), channels (if colored)
+  h, w, _ = bg_img.shape # Extract height (rows), width (cols), channels (if colored)
   fg_img = cv2.resize(fg_img, (w, h), interpolation=cv2.INTER_AREA)
   extruded_fg_img = cv2.resize(extruded_fg_img, (w, h), interpolation=cv2.INTER_AREA)
   logger.info(f"Background image size is {bg_img.shape}")
@@ -116,7 +134,7 @@ def main():
   # 
   # NOTE: Inverted grayscale is gray_r, which make extruded areas concave 
   #       rather than the desired convex appearance
-  save_img(extruded_fg_img, fname='extruded_fg_img.png')
+  save_img(extruded_fg_img, 'extruded_fg_img.png')
 
   # Read the extruded image back in from disc using opencv to prepare for color transferring
   extruded_fg_img = cv2.imread('extruded_fg_img.png')
@@ -158,9 +176,19 @@ def main():
     ], column_limit=3)
 
   # Save final result to file
-  # TODO Change output path to be relative to input path and configurable via args
-  outpath = 'blended.png'
-  save_img(blended_img, fname='blended.png')
+  # Create directory if it doesn't exit
+  dir_name = os.path.dirname(args.outpath)
+  dir_exists = os.path.exists(dir_name)
+  if not dir_exists:
+    os.makedirs(dir_name)
+
+  outpath = args.outpath
+
+  # Determine whether a file was specified in the outpath arg
+  if os.path.splitext(args.outpath)[1] == '':
+    outpath = os.path.join(outpath, 'blended.png')
+
+  save_img(blended_img, outpath)
   logger.info(f"See {outpath}")
 
 if __name__ == "__main__":
